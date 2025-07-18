b0ae3ec8be1959eca625d54fe16a3b01fb571fb7
Adam Jin <jinwoonggyu@gmail.com>
2025-07-18 21:44:54 +0200

    chore: ignore firebase namespace warnings

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+// https://rnfirebase.io/migrating-to-v22#switching-off-warning-logs
+global.RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
 AppRegistry.registerComponent(appName, () => App);
